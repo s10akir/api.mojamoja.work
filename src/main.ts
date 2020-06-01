fcf56c8cdc2caac21e4afee334dc4390cb335897
Akira Shinohara <k017c1067@it-neec.jp>
2020-06-01 23:14:46 +0900

    feat: create web api server

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
-function greet(target: string): string {
-  return `Hello, ${target}!`;
-}
+import express from "express";
 
-console.log(greet("World"));
+const app: express.Express = express();
+
+// Allow CORS
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_, res) => {
+  res.json({ message: "Hello World" });
+});
+
+const port: number = Number(process.env.PORT) || 3000;
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
