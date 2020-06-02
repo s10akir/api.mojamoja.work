f500fc4f0e26d35fd20304b38961fdaf7fad3c9b
Akira Shinohara <k017c1067@it-neec.jp>
2020-06-02 14:28:56 +0900

    feat: add honda variations

--- src/lib/honda/index.ts
+++ src/lib/honda/index.ts
@@ -17,7 +17,85 @@ export const Honda: Honda = {
 俺の勝ち！
 それで勝てると思ってるんやったら俺がずっと勝ちますよ。
 ほな、注ぎます。`,
-      imageUrl: `${UrlPrefix}/honda_1.png`,
+      imageUrl: `${UrlPrefix}/honda_01.png`,
+    },
+    {
+      message: `*Y O U  L O S E*
+俺の勝ち！
+何事も準備がすべて。それを怠っている事がバレてますよ。
+ほな、注ぎます。`,
+      imageUrl: `${UrlPrefix}/honda_02.png`,
+    },
+    {
+      message: `*Y O U  L O S E*
+俺の勝ち！
+何事も準備がすべて。それを怠っている事がバレてますよ。
+ほな、注ぎます。`,
+      imageUrl: `${UrlPrefix}/honda_03.png`,
+    },
+    {
+      message: `もと美味しくなったペプシジャパンコーラ、飲んでみたくないですか？
+それやったら買ってください。`,
+      imageUrl: `${UrlPrefix}/honda_04.png`,
+    },
+    {
+      message: `あなたの分はありません。`,
+      imageUrl: `${UrlPrefix}/honda_05.png`,
+    },
+    {
+      message: `*Y O U  L O S E*
+俺の勝ち！
+貴方の考えてることくらい僕にはお見通しです。
+ほな、注ぎます`,
+      imageUrl: `${UrlPrefix}/honda_06.png`,
+    },
+    {
+      message: `*Y O U  L O S E*
+俺の勝ち！
+その程度の気持ちで勝てるとでも思ったんですか？
+ちゃんと練習してきてください
+ほな、注ぎます`,
+      imageUrl: `${UrlPrefix}/honda_07.png`,
+    },
+    {
+      message: `*Y O U  L O S E*
+俺の勝ち！
+一年間何やってたんですか？
+これはじゃんけんに対する意識の差です。
+ほな、注ぎます`,
+      imageUrl: `${UrlPrefix}/honda_08.png`,
+    },
+    {
+      message: `*Y O U  L O S E*
+俺の勝ち！
+一年間何やってたんですか？
+これはじゃんけんに対する意識の差です。
+ほな、注ぎます`,
+      imageUrl: `${UrlPrefix}/honda_09.png`,
+    },
+    {
+      message: `*Y O U  W I N*
+俺の負け！
+今日は負けを認めます。
+ただ、勝ち逃げは許しませんよ
+ほな、注ぎます`,
+      imageUrl: `${UrlPrefix}/honda_10.png`,
+    },
+    {
+      message: `*Y O U  W I N*
+俺の負け！
+今日は負けを認めます。
+ただ、勝ち逃げは許しませんよ
+ほな、注ぎます`,
+      imageUrl: `${UrlPrefix}/honda_11.png`,
+    },
+    {
+      message: `*Y O U  W I N*
+俺の負け！
+今日は負けを認めます。
+ただ、勝ち逃げは許しませんよ
+ほな、注ぎます`,
+      imageUrl: `${UrlPrefix}/honda_12.png`,
     },
   ],
   random: function () {
